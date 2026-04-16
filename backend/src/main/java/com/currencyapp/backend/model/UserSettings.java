6e68bf0c2a1f322a7d9c75face4aa138f5718c7e
VaclavMoucha <vaclav8moucha@gmail.com>
2026-04-16 18:00:54 +0200

    add validation annotations to UserSettings fields

--- backend/src/main/java/com/currencyapp/backend/model/UserSettings.java
+++ backend/src/main/java/com/currencyapp/backend/model/UserSettings.java
@@ -4,11 +4,15 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserSettings {
+    @NotBlank(message = "Preferred currency is required")
     private String preferredCurrency;
+    @NotEmpty(message = "Selected currencies cannot be empty")
     private List<String> selectedCurrencies;
+
 }
